fbfabb19834aaca769df118cff464e830c35b88e
WhiteFalcon121 <58865937+WhiteFalcon121@users.noreply.github.com>
2020-06-18 18:13:22 +0100

    Update code with fixes in variable names (list1)

--- lowest_in_list.py
+++ lowest_in_list.py
@@ -1,14 +1,15 @@
 def find_lowest(list_name):
-  compars = 1
-  lowest = list1[0]
-  for x in list_name:
-    if compars == len(list_name):
-      break
-    next_index = list1.index(x) + 1
-    if list_name[next_index] < x and list_name[next_index] < lowest:
-      lowest = list_name[next_index]
-    compars = compars + 1
-  return lowest
-
-list1 = [2, 5, 1, 10, 1]
-print(find_lowest(list1))
+    compars = 1
+    lowest = list_name[0]
+    for x in list_name:
+        #print('compars:', compars)
+        #print('x: ', x)
+        if compars == len(list_name):
+            break
+        next_index = list_name.index(x) + 1
+        #print('next_index: ', next_index)
+        #print('next one is: ', list_name[next_index])
+        if list_name[next_index] < x and list_name[next_index] < lowest:
+            lowest = list_name[next_index]
+        compars += 1
+    return lowest
