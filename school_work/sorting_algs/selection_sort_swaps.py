53163d37b9951d000cc8e7dc0ac88b7def05295c
WhiteFalcon121 <t00171@reading-school.co.uk>
2020-06-20 17:45:15 +0100

    make selection into function

--- school_work/sorting_algs/selection_sort_swaps.py
+++ school_work/sorting_algs/selection_sort_swaps.py
@@ -11,29 +11,26 @@ def find_lowest(list_name):
         compars += 1
     return lowest
 
+def selection_sort(list1):
+    i = 0
+    list2 = list(list1)
+    for i in range(len(list1)):
+        list2 = list1[i:]
+        lowest = find_lowest(list2)
+        lowest_index = list1.index(lowest)
+        if lowest_index!= i:
+            list1[i], list1[lowest_index] = list1[lowest_index], list1[i]
+        i +=1
+
+        newlist = []
+        num = 0
+        for i in list1:
+            if list1.count(i) > 1 and i in newlist:
+                list1.pop(num)
+                list1.insert(list1.index(i)+1, i)
+            newlist.append(i)
+            num += 1
+    return list1
+
 list1 = [5, 4, 3, 2, 1, 2, 5]
-# find lowest, swap it
-# find lowest in sublist
-
-print('Sorting this list:', list1)
-
-i = 0
-list2 = list(list1)
-for i in range(len(list1)):
-    list2 = list1[i:]
-    lowest = find_lowest(list2)
-    lowest_index = list1.index(lowest)
-    if lowest_index!= i:
-        list1[i], list1[lowest_index] = list1[lowest_index], list1[i]
-    i +=1
-
-    newlist = []
-    num = 0
-    for i in list1:
-        if list1.count(i) > 1 and i in newlist:
-            list1.pop(num)
-            list1.insert(list1.index(i)+1, i)
-        newlist.append(i)
-        num += 1
-
-print('Result: ', list1)
+print(selection_sort(list1))
