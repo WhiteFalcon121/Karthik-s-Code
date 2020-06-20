c18c2b82521d9244dd9cd75a990711cef607d8d3
WhiteFalcon121 <t00171@reading-school.co.uk>
2020-06-20 17:03:26 +0100

    bubble sort into function

--- school_work/sorting_algs/bubble_sort.py
+++ school_work/sorting_algs/bubble_sort.py
@@ -1,16 +1,26 @@
-list1 = [1, 3, 4, 5, 2, 7, 8, 9, 10]
-print(list1)
+def bubble_sort(list1):
+    for i in range(0, len(list1)-1):
+      for x in list1:
+        next_index = list1.index(x) + 1
+        if next_index == len(list1):
+          break
+        'print(next_index)'
+        next_one = list1[next_index]
+        if x > next_one:
+          list1[next_index] = x
+          list1[list1.index(x)] = next_one
+        print(list1)
 
-for i in range(0, len(list1)-1):
-  for x in list1:
-    next_index = list1.index(x) + 1
-    if next_index == len(list1):
-      break
-    'print(next_index)'
-    next_one = list1[next_index]
-    if x > next_one:
-      list1[next_index] = x
-      list1[list1.index(x)] = next_one
-    print(list1)
+    newlist = []
+    num = 0
+    for i in list1:
+        if list1.count(i) > 1 and i in newlist:
+            list1.pop(num)
+            list1.insert(list1.index(i)+1, i)
+        newlist.append(i)
+        num += 1
 
-  print(list1)
+    return list1
+
+list1 = [5, 2, 3, 1, 4, 14, 0.3, 2, 4, 2, 4]
+print(bubble_sort(list1))
