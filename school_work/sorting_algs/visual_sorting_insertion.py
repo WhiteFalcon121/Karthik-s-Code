06674079e59b29627b4eb017e6fc97e30519e142
WhiteFalcon121 <t00171@reading-school.co.uk>
2020-06-20 17:42:20 +0100

    refine insertion sort with functions

--- school_work/sorting_algs/visual_sorting_insertion.py
+++ school_work/sorting_algs/visual_sorting_insertion.py
@@ -26,9 +26,40 @@ def drawlist(listname, space_counter, color_counter):
         space_counter = space_counter + 20
         color_counter = color_counter + 30
 
-    pygame.time.wait(100)
+    #pygame.time.wait(100)
     gameDisplay.fill((0, 0, 1))
 
+def insertion_sort(list1):
+    num1 = 0
+    for x in list1:
+        numsbefore = list1.index(x)
+        currentnum = x
+        if numsbefore != 0:
+            for x in list1[:numsbefore]:
+                if currentnum < x:
+                    num1 = num1 + 1
+                if num1 == len(list1[:numsbefore]):
+                    list1.remove(currentnum)
+                    list1.insert(0, currentnum)
+                    num1 = 0
+                    continue
+                if currentnum < x and list1.index(currentnum) > list1.index(x):
+                    list1.remove(currentnum)
+                    list1.insert(list1.index(x), currentnum)
+                num1 = 0
+
+        newlist = []
+        num = 0
+        for i in list1:
+            if list1.count(i) > 1 and i in newlist:
+                list1.pop(num)
+                list1.insert(list1.index(i)+1, i)
+            newlist.append(i)
+            num += 1
+
+        drawlist(list1, space_counter, color_counter)
+
+    return list1
 #set up
 
 gameDisplay = pygame.display.set_mode((canvas_x, canvas_y))
@@ -43,34 +74,9 @@ while running == 1:
                 pygame.quit()
                 quit()
 
-    for x in list1:
-        numsbefore = list1.index(x)
-        currentnum = x
-        if numsbefore != 0:
-            for x in list1[:numsbefore]: #for everything up to x
-                if currentnum < x:
-                    num1 = num1 + 1
-                if num1 == len(list1[:numsbefore]): #if currentnum is smaller than ALL of numbers before, put it at index 0
-                    list1.remove(currentnum)
-                    list1.insert(0, currentnum)
-                    num1 = 0
-                    #continue #skip next section
-                if currentnum < x and list1.index(currentnum) > list1.index(x):
-                    list1.remove(currentnum)
-                    list1.insert(list1.index(x), currentnum)
-            num1 = 0 #I forgot to reset it
-
-        newlist = []
-        num = 0
-        for i in list1:
-            if list1.count(i) > 1 and i in newlist:
-                list1.pop(num)
-                list1.insert(list1.index(i)+1, i)
-            newlist.append(i)
-            num += 1
-
-        drawlist(list1, space_counter, color_counter)
+    insertion_sort(list1)
 
     #print(list1)
     pygame.time.wait(100)
+    print("Result: ", list1)
     running = False
