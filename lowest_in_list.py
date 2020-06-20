445404a4dfe54083eb31d7ef156b3f4b12936d48
WhiteFalcon121 <58865937+WhiteFalcon121@users.noreply.github.com>
2020-06-20 09:06:04 +0100

    remove print statements

--- lowest_in_list.py
+++ lowest_in_list.py
@@ -2,13 +2,9 @@ def find_lowest(list_name):
     compars = 1
     lowest = list_name[0]
     for x in list_name:
-        #print('compars:', compars)
-        #print('x: ', x)
         if compars == len(list_name):
             break
         next_index = list_name.index(x) + 1
-        #print('next_index: ', next_index)
-        #print('next one is: ', list_name[next_index])
         if list_name[next_index] < x and list_name[next_index] < lowest:
             lowest = list_name[next_index]
         compars += 1
