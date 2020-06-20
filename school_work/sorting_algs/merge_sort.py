2b0ee6973196c951244eb12007d3ca79e9cc141a
WhiteFalcon121 <karkau007@gmail.com>
2020-06-20 11:18:40 +0100

    add merge_sort code

---
+++ school_work/sorting_algs/merge_sort.py
@@ -0,0 +1,38 @@
+def merge_sort(list_name):
+    if len(list_name) > 1:
+        mid = len(list_name)//2
+        left = list_name[:mid]
+        right = list_name[mid:]
+        print(left, right)
+        # recursive
+        merge_sort(left)
+        merge_sort(right)
+
+        # to go through halves
+        i = 0
+        j = 0
+
+        # to go through main list
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                list_name[k] = left[i]
+                i+=1
+            else:
+                list_name[k] = right[j]
+                j+= 1
+            k+=1
+
+        while i < len(left):
+            list_name[k] = left[i]
+            i+=1
+            k+=1
+        while j < len(right):
+            list_name[k] = right[j]
+            j+=1
+            k+=1
+    return list_name
+
+list1 = [2, 3, 7, 1, 5]
+print(merge_sort(list1))
