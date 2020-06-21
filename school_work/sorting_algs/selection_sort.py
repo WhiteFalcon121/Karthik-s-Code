785f7a8b5536ab32fdf9363d3e22cf3787e4e3d4
WhiteFalcon121 <karkau007@gmail.com>
2020-06-21 09:16:57 +0100

    update with new lowest function

--- school_work/sorting_algs/selection_sort.py
+++ school_work/sorting_algs/selection_sort.py
@@ -5,10 +5,8 @@ def find_lowest(list_name):
     for x in list_name:
         if compars == len(list_name):
             break
-        next_index = list_name.index(x) + 1
-        if list_name[next_index] < x and list_name[next_index] < lowest:
-            lowest = list_name[next_index]
-        compars += 1
+        if x < lowest:
+            lowest = x
     return lowest
 
 def selection_sort(list1, reverse):
