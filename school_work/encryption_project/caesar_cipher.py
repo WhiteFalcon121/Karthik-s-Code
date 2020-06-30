3d9d334093e2049dfd31fba46488554914f255a8
WhiteFalcon121 <karkau007@gmail.com>
2020-06-30 17:30:26 +0100

    encrypt function in caesar cipher

--- school_work/encryption_project/caesar_cipher.py
+++ school_work/encryption_project/caesar_cipher.py
@@ -1,7 +1,19 @@
 import random
-key = random.randint(0, 26) #inclusive range
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-'N', 'O', 'P', 'Q', 'R', 'S,' 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-plaintext = str(input("What message would you like to encrypt?").upper())
-print(plaintext)
+def encrypt(plaintext):
+    key = random.randint(0, 25) #inclusive range - if it was 26, the letter would be the same
+    ciphertext = []
+    for i in plaintext:
+        alphabet_index = alphabet.index(i)
+        new_key = alphabet_index + key
+        if new_key >= 26:
+            new_key = (alphabet_index + key) - 26
+        print(new_key)
+        ciphertext.append(alphabet[new_key])
+    ciphertext = ("".join(ciphertext))
+    return ciphertext, key
+
+message = str(input("What message would you like to encrypt?").upper())
+print(encrypt(message))
