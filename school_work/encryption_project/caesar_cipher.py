d13fcedbd91216716dae9c6e395de24d9372b67b
WhiteFalcon121 <karkau007@gmail.com>
2020-06-30 17:50:18 +0100

    add decryption function and ui

--- school_work/encryption_project/caesar_cipher.py
+++ school_work/encryption_project/caesar_cipher.py
@@ -10,10 +10,28 @@ def encrypt(plaintext):
         new_key = alphabet_index + key
         if new_key >= 26:
             new_key = (alphabet_index + key) - 26
-        print(new_key)
         ciphertext.append(alphabet[new_key])
     ciphertext = ("".join(ciphertext))
     return ciphertext, key
 
-message = str(input("What message would you like to encrypt?").upper())
-print(encrypt(message))
+def decrypt(ciphertext, key):
+    plaintext = []
+    for i in ciphertext:
+        alphabet_index = alphabet.index(i)
+        real_index = alphabet_index - key
+        if real_index < 0:
+            real_index = 26 - real_index
+        plaintext.append(alphabet[real_index])
+    plaintext = ("".join(plaintext))
+    return plaintext
+
+option = str(input("Decrypt or encrypt? (d or e)"))
+if option == "D" or option == "d":
+    ciphertext = input("Enter ciphertext: ").upper()
+    key = int(input("Enter key: "))
+    print(decrypt(ciphertext, key))
+elif option == "E" or option == "e":
+    message = str(input("Enter message to be encrypted: ").upper())
+    print(encrypt(message))
+else:
+    print("Not an option.")
