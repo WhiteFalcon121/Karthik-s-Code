78d9463a1757b8f54bee3cdc5302d272fa5e7a27
HOME <home@HOMEs-MacBook-Pro.local>
2020-07-08 14:51:29 +0100

    update otp with fix to running over alphabet and low key length

--- school_work/encryption_project/encryption_input_trial.py
+++ school_work/encryption_project/encryption_input_trial.py
@@ -3,26 +3,10 @@ from time import *
 from caesar_cipher import decrypt
 
 mouse = Controller()
-key_list = []
-new_key_list = []
 
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-for i in range(0, 5):
-    key_list.append(mouse.position)
-    sleep(0.4)
-
-for i in key_list:
-    i = list(i)
-    if 0.0 in i: #don't want characters to stay same
-        i.remove(0.0)
-    for x in i:
-        # here is where they should be converted to double digits
-        new_key_list.append(round(x/100))
-
-print(new_key_list)
-key = new_key_list
 def encrypt(plaintext, key):
     ciphertext = []
     pos = -1
@@ -32,12 +16,33 @@ def encrypt(plaintext, key):
             alphabet_index = alphabet.index(i)
             new_key = alphabet_index + key[pos]
             if new_key >= 26:
-                new_key = (alphabet_index + key) - 26
+                new_key = (alphabet_index + key[pos]) - 26
             ciphertext.append(alphabet[new_key])
         else:
             ciphertext.append(i) # for special characters
     ciphertext = ("".join(ciphertext))
     return ciphertext, key[:len(ciphertext)]
 
-message = "abc"
+def make_key(message):
+    key_list = []
+    new_key_list = []
+    for i in range(0, 5):
+        key_list.append(mouse.position)
+        sleep(0.4)
+
+    for i in key_list:
+        i = list(i)
+        if 0.0 in i: #don't want characters to stay same
+            i.remove(0.0)
+        for x in i:
+            # here is where they should be converted to double digits
+            new_key_list.append(round(x/100))
+
+    while len(new_key_list) < len(message):
+        new_key_list = 2*new_key_list #repeat key if not as long as the message 
+
+    return new_key_list
+
+message = "abcdefghijklmopqrstuvwxyzabc"
+key = make_key(message)
 print(encrypt(message.upper(), key))
